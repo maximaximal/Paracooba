67fce4b12182cda61b6244e159d999628dc8477d
Max Heisinger <mail@maximaximal.com>
2020-01-09 17:50:31 +0000

    Implement assignment de-serializer in shared header and add test.

--- paracuber/libparacuber/include/paracuber/assignment_serializer.hpp
+++ paracuber/libparacuber/include/paracuber/assignment_serializer.hpp
@@ -3,13 +3,15 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <vector>
 
 namespace paracuber {
 
 #define PC_SHIFT(X) (((solver.val(i + X + 1)) & 0b00000001) << (7 - X))
-#define PC_SHIFT_CASE(X)                                        \
-  case X:                                                       \
-    b |= (((solver.val(i + 1)) & 0b00000001) << (7 - (i % 8))); \
+#define PC_SHIFT_CASE(X)                                                       \
+  case X:                                                                      \
+    b |=                                                                       \
+      (((solver.val(i + 1)) & 0b00000001) << (7 - (i % 8) - (8 - remaining))); \
     ++i;
 
 /** @brief Serialise the given solver results into an assignment vector.
@@ -43,7 +45,8 @@ SerializeAssignment(const int varCount,
 
   if(varCount > 0) {
     uint8_t b = 0;
-    switch(varCount - i) {
+    size_t remaining = varCount - i;
+    switch(remaining) {
       PC_SHIFT_CASE(7)
       PC_SHIFT_CASE(6)
       PC_SHIFT_CASE(5)
@@ -60,6 +63,50 @@ SerializeAssignment(const int varCount,
     // No error handling is done here though, this should be handled from
     // outside.
   }
+}
+
+template<typename AssignmentVector>
+inline void
+DeSerializeSingleToAssignment(AssignmentVector& target, uint8_t next, size_t n)
+{
+  // Get next assignments, depending on remaining size. At the very end,
+  // only valid variables shall be extracted.
+  switch(n) {
+    default:
+      target.push_back(static_cast<bool>(next & 0b10000000u));
+    case 7:
+      target.push_back(static_cast<bool>(next & 0b01000000u));
+    case 6:
+      target.push_back(static_cast<bool>(next & 0b00100000u));
+    case 5:
+      target.push_back(static_cast<bool>(next & 0b00010000u));
+    case 4:
+      target.push_back(static_cast<bool>(next & 0b00001000u));
+    case 3:
+      target.push_back(static_cast<bool>(next & 0b00000100u));
+    case 2:
+      target.push_back(static_cast<bool>(next & 0b00000010u));
+    case 1:
+      target.push_back(static_cast<bool>(next & 0b00000001u));
+    case 0:
+      break;
+  }
+}
+
+template<typename AssignmentVector>
+inline std::vector<uint8_t>
+DeSerializeToAssignment(AssignmentVector& source, size_t varCount)
+{
+  std::vector<uint8_t> out;
+  out.reserve(varCount);
+
+  size_t i;
+  for(i = 0; varCount > 8; ++i, varCount -= 8) {
+    DeSerializeSingleToAssignment(out, source[i], varCount);
+  }
+
+  DeSerializeSingleToAssignment(out, source[i], varCount);
+  return std::move(out);
 }
 
 #undef PC_SHIFT
--- paracuber/test/assignment_serializer_test.cpp
+++ paracuber/test/assignment_serializer_test.cpp
@@ -1,51 +1,68 @@
 #include <catch2/catch.hpp>
+#include <initializer_list>
 #include <paracuber/assignment_serializer.hpp>
 
 using namespace paracuber;
 
 struct Solver
 {
+  explicit Solver(const std::vector<uint8_t>& assignments)
+    : assignments(assignments)
+  {}
+
   bool val(int lit) const
   {
     // Literals start at 1, as they also represent negations in SAT solvers.
     lit -= 1;
 
     REQUIRE(lit < getVarCount());
-    return assignment[lit];
+    return assignments[lit];
   }
-  std::vector<bool> assignment;
-  int getVarCount() const { return assignment.size(); }
+  const std::vector<uint8_t> assignments;
+  int getVarCount() const { return assignments.size(); }
 };
 
 struct Test
 {
-  Solver solver;
+  explicit Test(std::initializer_list<uint8_t> assignments,
+                std::initializer_list<uint8_t> expected)
+    : assignments(assignments)
+    , expected(expected)
+    , solver(this->assignments)
+  {}
+  std::vector<uint8_t> assignments;
   std::vector<uint8_t> expected;
+  Solver solver;
 };
 
 #define T true
 #define F false
 
-TEST_CASE("Serialize SAT Assignment")
+TEST_CASE("(Un-)Serialize SAT Assignment")
 {
   auto test = GENERATE(
-    Test{ { { T, F, F, F, T, T, T, T /* 1st byte */, F, F, F, T, T } },
-          { 0b10001111, 0b00011000 } },
-    Test{ { { T, F, F, F, T, T, T, T /* 1st byte */, T } },
-          { 0b10001111, 0b10000000 } },
-    Test{ { { T, F, F, F, T, T, T, T /* 1st byte */, F, F, F, F, F, F, F, T } },
-          { 0b10001111, 0b00000001 } },
-    Test{ { { T, F, F, F, T, T, T, T /* 1st byte */,
-              F, F, F, F, F, F, F, T /* 2nd byte */,
-              F, T, F, T, F, T, F, T } },
-          { 0b10001111, 0b00000001, 0b01010101 } },
-    Test{ { { T, F, F, F, T, T, T, T /* 1st byte */ } }, { 0b10001111 } });
+    Test({ T, F, F, F, T, T, T, T /* 1st byte */, F, F, F, T, T },
+         { 0b10001111, 0b00000011 }),
+    Test({ T, F, F, F, T, T, T, T /* 1st byte */, T },
+         { 0b10001111, 0b00000001 }),
+    Test({ T, F, F, F, T, T, T, T /* 1st byte */, F, F, F, F, F, F, F, T },
+         { 0b10001111, 0b00000001 }),
+    Test({ T, F, F, F, T, T, T, T /* 1st byte */,
+           F, F, F, F, F, F, F, T /* 2nd byte */,
+           F, T, F, T, F, T, F, T },
+         { 0b10001111, 0b00000001, 0b01010101 }),
+    Test({ T, F, F, F, T, T, T, T /* 1st byte */ }, { 0b10001111 }));
 
-  CAPTURE(test.solver.getVarCount());
+  uint32_t varCount = test.solver.getVarCount();
+  CAPTURE(varCount);
 
-  std::vector<uint8_t> actual((test.solver.getVarCount() - 1) / 8 + 1);
+  std::vector<uint8_t> actual((varCount - 1) / 8 + 1);
 
-  SerializeAssignment(test.solver.getVarCount(), test.solver, actual);
+  SerializeAssignment(varCount, test.solver, actual);
 
   REQUIRE(test.expected == actual);
+
+  // Other direction.
+  auto calculatedAssignments = DeSerializeToAssignment(actual, varCount);
+  REQUIRE(test.assignments == calculatedAssignments);
 }
