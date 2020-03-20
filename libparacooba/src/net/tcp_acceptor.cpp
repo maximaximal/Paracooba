6d8b50c702b4bb8f4db739e92e03caf90094d09f
Max Heisinger <mail@maximaximal.com>
2020-03-20 18:05:27 +0100

    Add TCPAcceptor class.

---
+++ libparacooba/src/net/tcp_acceptor.cpp
@@ -0,0 +1,84 @@
+#include "../../include/paracooba/net/tcp_acceptor.hpp"
+#include "../../include/paracooba/net/connection.hpp"
+#include <boost/system/error_code.hpp>
+
+namespace paracooba {
+namespace net {
+TCPAcceptor::State::State(
+  boost::asio::io_service& ioService,
+  boost::asio::ip::tcp::endpoint endpoint,
+  LogPtr log,
+  ConfigPtr config,
+  messages::MessageReceiver& msgReceiver,
+  messages::JobDescriptionReceiverProvider& jdReceiverProvider)
+  : ioService(ioService)
+  , acceptor(ioService, endpoint)
+  , log(log)
+  , logger(log->createLogger("TCPAcceptor"))
+  , config(config)
+  , msgReceiver(msgReceiver)
+  , jdReceiverProvider(jdReceiverProvider)
+{}
+
+TCPAcceptor::TCPAcceptor(
+  boost::asio::io_service& ioService,
+  boost::asio::ip::tcp::endpoint endpoint,
+  LogPtr log,
+  ConfigPtr config,
+  messages::MessageReceiver& msgReceiver,
+  messages::JobDescriptionReceiverProvider& jdReceiverProvider)
+  : m_state(std::make_shared<State>(ioService,
+                                    endpoint,
+                                    log,
+                                    config,
+                                    msgReceiver,
+                                    jdReceiverProvider))
+{}
+TCPAcceptor::~TCPAcceptor() {}
+
+void
+TCPAcceptor::startAccepting()
+{
+  (*this)(boost::system::error_code());
+}
+
+#include <boost/asio/yield.hpp>
+
+void
+TCPAcceptor::operator()(const boost::system::error_code& ec)
+{
+  if(!ec) {
+    reenter(this)
+    {
+      if(!newConnection())
+        makeNewConnection();
+
+      yield acceptor().async_accept(newConnection()->socket(), *this);
+
+      newConnection()->socket().non_blocking(true);
+
+      newConnection()->readHandler();
+
+      newConnection().reset();
+    }
+  } else {
+    PARACOOBA_LOG(logger(), LocalError)
+      << "Error during accepting new connections! Error: " << ec.message();
+  }
+}
+
+#include <boost/asio/unyield.hpp>
+
+void
+TCPAcceptor::makeNewConnection()
+{
+  newConnection() =
+    std::make_unique<Connection>(ioService(),
+                                 log(),
+                                 config(),
+                                 messageReceiver(),
+                                 jobDescriptionReceiverProvider());
+}
+
+}
+}
