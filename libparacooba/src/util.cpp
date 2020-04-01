18f31ce2ed7903c72dc8589046feffc28eda0faa
Max Heisinger <mail@maximaximal.com>
2020-04-01 18:53:19 +0200

    Set byte suffix to B for byte pretty print.

--- libparacooba/src/util.cpp
+++ libparacooba/src/util.cpp
@@ -9,7 +9,7 @@ std::string
 BytePrettyPrint(size_t bytes)
 {
   auto base = (double)std::log(bytes) / (double)std::log(1024);
-  const char* suffixArr[] = { " ", "kiB", "MiB", "GiB", "TiB", "PiB" };
+  const char* suffixArr[] = { "B", "kiB", "MiB", "GiB", "TiB", "PiB" };
   return std::to_string(
            (size_t)std::round(std::pow(1024, base - std::floor(base)))) +
          suffixArr[(size_t)std::floor(base)];
