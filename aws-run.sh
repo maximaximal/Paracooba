17bd0c8b469b2a2ca39a8acb9277174e0f67547d
Max Heisinger <mail@maximaximal.com>
2020-04-08 17:43:52 +0200

    Add debugging options and make without -j to preserve ressources.

--- aws-run.sh
+++ aws-run.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [ -z ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS+x} ]; then
     aws s3 cp s3://${S3_BKT}/${COMP_S3_PROBLEM_PATH} $DIR/build/problem.cnf
-    $DIR/build/parac $DIR/build/problem.cnf $@
+    $DIR/build/parac $DIR/build/problem.cnf --debug $@
 else
-    $DIR/build/parac --daemon $Q
+    $DIR/build/parac --daemon --debug $Q
 fi
