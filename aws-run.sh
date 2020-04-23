dcff0b4337269c7860debaae3d4a39ca4aef526d
Max Heisinger <mail@maximaximal.com>
2020-04-23 14:30:58 +0200

    Half thread count on daemon nodes on AWS.

--- aws-run.sh
+++ aws-run.sh
@@ -2,9 +2,17 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+DAEMON_THREADS=$(grep -c ^processor /proc/cpuinfo)
+let DAEMON_THREADS=DAEMON_THREADS/2
+
 if [ -z ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS+x} ]; then
     aws s3 cp s3://${S3_BKT}/${COMP_S3_PROBLEM_PATH} $DIR/build/problem.cnf
     $DIR/build/parac $DIR/build/problem.cnf --debug --network-debug $@
 else
-    $DIR/build/parac --daemon --debug --network-debug $Q
+    $DIR/build/parac --daemon --threads $DAEMON_THREADS --debug --network-debug $Q
+fi
+
+if [ $? -ne 0 ]; then
+    echo "Return code of parac was not successful! Try to print coredump."
+    coredumpctl dump
 fi
