b6673c21fc0befb7ac6249d1182a13a28edc5e7b
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2020-05-01 07:30:18 +0200

    use march script

--- aws-run.sh
+++ aws-run.sh
@@ -7,12 +7,13 @@ let DAEMON_THREADS=DAEMON_THREADS/2
 
 if [ -z ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS+x} ]; then
     aws s3 cp s3://${S3_BKT}/${COMP_S3_PROBLEM_PATH} $DIR/build/problem.cnf
-    $DIR/build/parac --cadical-cubes  --cadical-cubes-depth 17 --cadical-minimal-cubes-depth 12 --resplit-cubes $DIR/build/problem.cnf $@
+    $DIR/scripts/generate-cubes.sh $DIR/build/problem.cnf > $DIR/build/problem.icnf
+    $DIR/build/parac --resplit-cubes --cadical-cubes-depth 17 --cadical-minimal-cubes-depth 12 $DIR/build/problem.icnf $@
 else
-    $DIR/build/parac --resplit-cubes  --daemon --threads $DAEMON_THREADS $Q
+    $DIR/build/parac --daemon --resplit-cubes --threads $DAEMON_THREADS $Q
 fi
 
-# if [ $? -ne 0 ]; then
-#    echo "Return code of parac was not successful! Try to print coredump."
-#     coredumpctl dump
-# fi
+if [ $? -ne 0 ]; then
+    echo "Return code of parac was not successful! Try to print coredump."
+    coredumpctl dump
+fi
