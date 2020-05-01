9831655a85ee61bdf3a3a137803290f05533a9de
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2020-05-01 09:10:07 +0200

    fix script path

--- aws-run-march.sh
+++ aws-run-march.sh
@@ -7,7 +7,7 @@ let DAEMON_THREADS=DAEMON_THREADS/2
 
 if [ -z ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS+x} ]; then
     aws s3 cp s3://${S3_BKT}/${COMP_S3_PROBLEM_PATH} $DIR/build/problem.cnf
-    ./scripts/generate-cubes.sh $DIR/build/problem.cnf > $DIR/build/problem.icnf
+    $DIR/scripts/generate-cubes.sh $DIR/build/problem.cnf > $DIR/build/problem.icnf
     $DIR/build/parac --resplit-cubes --cadical-cubes-depth 17 --cadical-minimal-cubes-depth 12 $DIR/build/problem.icnf $@
 else
     $DIR/build/parac --daemon --resplit-cubes --threads $DAEMON_THREADS $Q
