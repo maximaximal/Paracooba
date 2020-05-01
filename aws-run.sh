afdb161d11dccf6b8cc4afdbe6e9cc0781565960
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2020-05-01 15:03:49 +0200

    split deeper initially

--- aws-run.sh
+++ aws-run.sh
@@ -7,12 +7,12 @@ let DAEMON_THREADS=DAEMON_THREADS/2
 
 if [ -z ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS+x} ]; then
     aws s3 cp s3://${S3_BKT}/${COMP_S3_PROBLEM_PATH} $DIR/build/problem.cnf
-    $DIR/build/parac $DIR/build/problem.cnf $@
+    $DIR/build/parac --cadical-cubes  --cadical-cubes-depth 17 --cadical-minimal-cubes-depth 12 --resplit-cubes $DIR/build/problem.cnf $@
 else
-    $DIR/build/parac --daemon --threads $DAEMON_THREADS $Q
+    $DIR/build/parac --resplit-cubes  --daemon --threads $DAEMON_THREADS $Q
 fi
 
-if [ $? -ne 0 ]; then
-    echo "Return code of parac was not successful! Try to print coredump."
-    coredumpctl dump
-fi
+# if [ $? -ne 0 ]; then
+#    echo "Return code of parac was not successful! Try to print coredump."
+#     coredumpctl dump
+# fi
