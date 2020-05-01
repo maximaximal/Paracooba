0183df85c32538187aedcf5b5bced7e30a987d5c
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2020-05-01 07:12:36 +0200

    add generate-cube script

---
+++ aws-run-march.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+DAEMON_THREADS=$(grep -c ^processor /proc/cpuinfo)
+let DAEMON_THREADS=DAEMON_THREADS/2
+
+if [ -z ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS+x} ]; then
+    aws s3 cp s3://${S3_BKT}/${COMP_S3_PROBLEM_PATH} $DIR/build/problem.cnf
+    ./scripts/generate-cubes.sh $DIR/build/problem.cnf > $DIR/build/problem.icnf
+    $DIR/build/parac $DIR/build/problem.cnf $@
+else
+    $DIR/build/parac --daemon --threads $DAEMON_THREADS $Q
+fi
+
+if [ $? -ne 0 ]; then
+    echo "Return code of parac was not successful! Try to print coredump."
+    coredumpctl dump
+fi
