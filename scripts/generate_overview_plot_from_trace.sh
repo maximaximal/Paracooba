336a5682c845dcd3ebcbbab5f8ce7be65b943389
Max Heisinger <mail@maximaximal.com>
2021-02-01 11:06:02 +0100

    Improve gnuplots

--- scripts/generate_overview_plot_from_trace.sh
+++ scripts/generate_overview_plot_from_trace.sh
@@ -23,19 +23,37 @@ ensure_command_available "gnuplot";
 T=$1
 N=$(distrac $T --print-node-count)
 
-echo "Working with $N nodes.";
+e>&2 cho "Working with $N nodes.";
 
-echo "Generate Offload Chart Data...";
+e>&2 cho "Generate Offload Chart Data...";
 
 mkdir /dev/shm/overview
 
-distrac --omit-csv-header $T -e "offload_task" | awk '{ print $1 " " $3 " 0 " $4 - $3 " " $7; }' > /dev/shm/overview/offloads.data
-distrac --omit-csv-header $T -e "finish_processing_task" | awk '{ print $1 " " $3 " " $8; }' > /dev/shm/overview/timings.data
+for (( i = 0; i < $N; i++ ))
+do
+    echo "\"Node $i\""
+    distrac --select-nodes $i --omit-csv-header $T -e "offload_task" | awk '{ print $1 " " $3 " 0 " $4 - $3 " " $7; }'
+    echo ""
+    echo ""
+done > /dev/shm/overview/offloads.data
 
-# for (( i = 0; i < $N; i++ ))
-# do
-# done
+for (( i = 0; i < $N; i++ ))
+do
+    echo "\"Node $i\""
+    distrac --select-nodes $i --omit-csv-header $T -e "finish_processing_task" | awk '{ print $1 " n" $3 " " $8; }'
+    echo ""
+    echo ""
+done > /dev/shm/overview/timings.data
 
-gnuplot -e 'offloads="/dev/shm/overview/offloads.data"' -e 'timings="/dev/shm/overview/timings.data"' $DIR/generate_overview_plot_from_trace_plt.gnuplot --persist
+for (( i = 0; i < $N; i++ ))
+do
+    >&2 echo "   Working on utilization data for internal node id $i"
+    echo "\"Node $i\""
+    distrac $T -e "worker_idle" "worker_working" --select-nodes $i --omit-csv-header | awk 'function alen(a, i, k) { k = 0; for(i in a) k++; if(k == 0) return 1; return k; }; function aworking(a, i, k) { k = 0; for(i in a) if(a[i]==1) k++; return k; }; $1 == "worker_idle" { print ($2-1) " n" $4 " " (aworking(w) / alen(w)); w[$5]=0; print $2 " n" $4 " " (aworking(w) / alen(w)); }; $1 == "worker_working" { print ($2-1) " n" $4 " " (aworking(w) / alen(w)); w[$5]=1; print $2 " n" $4 " " (aworking(w) / alen(w)) };'
+    echo ""
+    echo ""
+done > /dev/shm/overview/utilization.data
 
-#rm -rf /dev/shm/overview
+gnuplot -e 'utilization="/dev/shm/overview/utilization.data"' -e 'offloads="/dev/shm/overview/offloads.data"' -e 'timings="/dev/shm/overview/timings.data"' $DIR/generate_overview_plot_from_trace_plt.gnuplot > ${T}_plots.png
+
+rm -rf /dev/shm/overview
--- scripts/generate_overview_plot_from_trace_plt.gnuplot
+++ scripts/generate_overview_plot_from_trace_plt.gnuplot
@@ -1,16 +1,36 @@
-set multiplot layout 3,1
+# Higher DPI Plots:
+# https://unix.stackexchange.com/a/617970
+dpi = 300 ## dpi (variable)
+width = 250 ## mm (variable)
+height = 600 ## mm (variable)
 
-set palette defined ( 0 "red", 1 "yellow", 2 "cyan", 3 "blue", 4 "magenta")
+in2mm = 25.4 # mm (fixed)
+pt2mm = 0.3528 # mm (fixed)
+
+mm2px = dpi/in2mm
+ptscale = pt2mm*mm2px
+round(x) = x - floor(x) < 0.5 ? floor(x) : ceil(x)
+wpx = round(width * mm2px)
+hpx = round(height * mm2px)
+
+set terminal pngcairo size wpx,hpx fontscale ptscale linewidth ptscale pointscale ptscale
+
+set multiplot layout 4,1
+
+set palette maxcolors 2
 
 set lmargin at screen 0.1
 set rmargin at screen 0.9
 unset xtics
+set key autotitle columnheader
 
 set title "Task Offloads"
-plot offloads using 1:2:3:4 with vectors notitle
+plot for [i=0:*] offloads index i using 1:2:3:4:yticlabels(2) with vectors notitle
 set title "Utilization in the Moment of Offload"
-plot offloads using 1:5:2 with impulses lc palette notitle
+plot for [i=0:*] offloads index i using 1:5 with impulses title columnheader(1)
 set title "Task Runtime"
-plot timings using 1:3:2 with impulses lc palette notitle
+plot for [i=0:*] timings index i using 1:3 with impulses title columnheader(1)
+set title "Runner Workload"
+plot for [i=0:*] utilization index i using 1:3 with lines lw 2 title columnheader(1)
 
 unset multiplot
