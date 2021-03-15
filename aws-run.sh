d5e03220f8afa0d95f7b394756f27bba0deab930
Max Heisinger <mail@maximaximal.com>
2021-03-15 13:51:38 +0100

    Use NUM_PROCESSES in aws-run.sh

--- aws-run.sh
+++ aws-run.sh
@@ -2,14 +2,14 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-DAEMON_THREADS=$(grep -c ^processor /proc/cpuinfo)
-let DAEMON_THREADS=DAEMON_THREADS/2
+#DAEMON_THREADS=$(grep -c ^processor /proc/cpuinfo)
+#let DAEMON_THREADS=DAEMON_THREADS/2
 
 if [ -z ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS+x} ]; then
     aws s3 cp s3://${S3_BKT}/${COMP_S3_PROBLEM_PATH} $DIR/build/problem.cnf
-    $DIR/build/paracs --cadical-cubes --initial-cube-depth 17 --initial-minimal-cube-depth 12 --resplit $DIR/build/problem.cnf --worker $DAEMON_THREADS -d $@
+    $DIR/build/paracs --cadical-cubes --initial-cube-depth 17 --initial-minimal-cube-depth 12 --resplit $DIR/build/problem.cnf --worker $NUM_PROCESSES -d $@
 else
-    $DIR/build/paracs --worker $DAEMON_THREADS --known-remote ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS} -d $Q
+    $DIR/build/paracs --worker $NUM_PROCESSES --known-remote ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS} -d $Q
 fi
 
 # if [ $? -ne 0 ]; then
