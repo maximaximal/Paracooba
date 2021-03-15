5f5b5797a14176b57e8197e1313e7c4d16c97dd0
Max Heisinger <mail@maximaximal.com>
2021-03-15 16:10:44 +0100

    Do detection of main node like described on Github

--- aws-run.sh
+++ aws-run.sh
@@ -5,7 +5,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 #DAEMON_THREADS=$(grep -c ^processor /proc/cpuinfo)
 #let DAEMON_THREADS=DAEMON_THREADS/2
 
-if [ -z ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS+x} ]; then
+if [ "$AWS_BATCH_JOB_MAIN_NODE_INDEX" = "$AWS_BATCH_JOB_NODE_INDEX" ]; then
+    # Main node detected!
     ip=$(hostname -I | cut -d' ' -f1)
     echo "main IP: ${ip}"
     aws s3 cp s3://${S3_BKT}/${COMP_S3_PROBLEM_PATH} $DIR/build/problem.cnf
