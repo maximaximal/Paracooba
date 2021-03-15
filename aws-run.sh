27e62bedcf4046b8066745571377fd25fb20e41a
Max Heisinger <mail@maximaximal.com>
2021-03-15 16:40:31 +0100

    Listen on IPv4

--- aws-run.sh
+++ aws-run.sh
@@ -10,8 +10,9 @@ if [ "$AWS_BATCH_JOB_MAIN_NODE_INDEX" = "$AWS_BATCH_JOB_NODE_INDEX" ]; then
     ip=$(hostname -I | cut -d' ' -f1)
     echo "main IP: ${ip}"
     aws s3 cp s3://${S3_BKT}/${COMP_S3_PROBLEM_PATH} $DIR/build/problem.cnf
-    $DIR/build/paracs --cadical-cubes --initial-cube-depth 17 --initial-minimal-cube-depth 12 --resplit $DIR/build/problem.cnf --worker $NUM_PROCESSES -d $@
+    $DIR/build/paracs --cadical-cubes --initial-cube-depth 17 --initial-minimal-cube-depth 12 --resplit $DIR/build/problem.cnf --worker $NUM_PROCESSES -d --tcp-listen-address 0.0.0.0 $@
 else
+    echo "c DAEMON NODE: Trying to connect to IP ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS}"
     $DIR/build/paracs --worker $NUM_PROCESSES --known-remote ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS} -d $Q
 fi
 
