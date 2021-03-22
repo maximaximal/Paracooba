b5d2c5f21db409138f4911e87e6ba2c970f45c3e
Max Heisinger <mail@maximaximal.com>
2021-03-22 15:03:41 +0100

    Only issue a warning if ACK comes in twice

--- modules/communicator/tcp_connection.cpp
+++ modules/communicator/tcp_connection.cpp
@@ -890,13 +890,13 @@ TCPConnection::readHandler(boost::system::error_code ec,
         if(!handleReceivedACK(s->readHeader)) {
           parac_log(
             PARAC_COMMUNICATOR,
-            PARAC_LOCALERROR,
+            PARAC_LOCALWARNING,
             "Error in TCPConnection to endpoint {} (ID {}) readHandler when "
-            "reading ACK for message id {}!",
+            "reading ACK for message id {}! This could have been a wrongly "
+            "sent ACK, ignoring this mishap.",
             s->remote_endpoint(),
             s->remoteId(),
             s->readHeader.number);
-          return;
         }
       } else if(s->readHeader.kind == PARAC_MESSAGE_FILE) {
         yield async_read(*s->socket, BUF(s->readFileHeader), rh);
