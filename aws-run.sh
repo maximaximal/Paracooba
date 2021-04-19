7367742e670c9a1e1ac3de13766a732ffee7a7e2
Max Heisinger <mail@maximaximal.com>
2021-04-19 12:18:30 +0200

    Less positional arguments on AWS
    Change related to switch from entrypoint to CMD

--- aws-run.sh
+++ aws-run.sh
@@ -16,11 +16,11 @@ if [ "$AWS_BATCH_JOB_MAIN_NODE_INDEX" == "$AWS_BATCH_JOB_NODE_INDEX" ]; then
     # Main node detected!
     echo "main IP: ${ip}"
     aws s3 cp s3://${S3_BKT}/${COMP_S3_PROBLEM_PATH} $DIR/build/problem.cnf
-    time $DIR/build/paracs --cadical-cubes --initial-cube-depth 15 --initial-minimal-cube-depth 12 --resplit $DIR/build/problem.cnf --concurrent-cube-tree-count 4 --worker $nproc --id $id --tcp-listen-address 0.0.0.0 $@
+    time $DIR/build/paracs --cadical-cubes --initial-cube-depth 15 --initial-minimal-cube-depth 12 --resplit $DIR/build/problem.cnf --concurrent-cube-tree-count 4 --worker $nproc --id $id --tcp-listen-address 0.0.0.0
 else
     echo "c DAEMON NODE: Trying to connect to IP ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS} from local ip $ip"
     ping -c 5 ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS}
-    $DIR/build/paracs --worker $nproc --known-remote ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS} --auto-shutdown-after-finished-client --tcp-listen-address 0.0.0.0 --id $id $@
+    $DIR/build/paracs --worker $nproc --known-remote ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS} --auto-shutdown-after-finished-client --tcp-listen-address 0.0.0.0 --id $id
 fi
 
 # if [ $? -ne 0 ]; then
