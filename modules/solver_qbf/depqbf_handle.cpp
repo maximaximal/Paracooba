f1c0db578a564bf291d6e981691da6c66308b114
Max Heisinger <mail@maximaximal.com>
2021-05-18 17:07:27 +0200

    Embed DepQBF into solver module and link

--- modules/solver_qbf/depqbf_handle.cpp
+++ modules/solver_qbf/depqbf_handle.cpp
@@ -1,11 +1,20 @@
+extern "C" {
+#include <qdpll.h>
+}
+
 #include "depqbf_handle.hpp"
 
 namespace parac::solver_qbf {
-DepQBFHandle::DepQBFHandle(const Parser& parser) {}
+DepQBFHandle::DepQBFHandle(const Parser& parser)
+  : m_qdpll(qdpll_create(), &qdpll_delete) {}
 DepQBFHandle::~DepQBFHandle() {}
 
 void
-DepQBFHandle::assumeCube(const Cube& cube) {}
+DepQBFHandle::assumeCube(const Cube& cube) {
+  for(Literal lit : cube) {
+    qdpll_assume(m_qdpll.get(), lit);
+  }
+}
 
 parac_status
 DepQBFHandle::solve() {}
--- modules/solver_qbf/depqbf_handle.hpp
+++ modules/solver_qbf/depqbf_handle.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <memory>
+
 #include "generic_qbf_handle.hpp"
+
+struct QDPLL;
 
 namespace parac::solver_qbf {
 class Parser;
@@ -10,9 +14,10 @@ class DepQBFHandle : public GenericSolverHandle {
   DepQBFHandle(const Parser& parser);
   virtual ~DepQBFHandle();
 
-  virtual void assumeCube(const Cube &cube);
+  virtual void assumeCube(const Cube& cube);
   virtual parac_status solve();
 
   private:
+  std::unique_ptr<QDPLL, void (*)(QDPLL*)> m_qdpll;
 };
 }
