7682efe81edd9a42492da00a078d2fa692c14472
Max Heisinger <mail@maximaximal.com>
2021-08-12 14:18:48 +0200

    Improve QBF testing

--- test/test_qbf_exists_forall_unsat.cpp
+++ test/test_qbf_exists_forall_unsat.cpp
@@ -1,40 +1,106 @@
 #include <catch2/catch.hpp>
 
+#include <string_view>
+
 #include <paracooba/module.h>
 
 #include "mocks.hpp"
 
-static const char* test_qdimacs_str = R"(:p cnf 2 2
+static std::string test_qdimacs_str = R"(:p cnf 2 2
 e 1 0
 a 2 0
 2 0
 -2 0
 )";
 
-TEST_CASE("Single threaded QBF formula that goes EXISTS 1 FORALL 2",
+static std::string test_qdimacs_str_longer = R"(:p cnf 6 2
+e 1 0
+a 2 3 4 0
+e 5 6 0
+2 0
+-2 0
+)";
+
+static std::string test_qdimacs_str_qbffam_parity_10 = R"(:p cnf 20 38
+e 1 2 3 4 5 6 7 8 9 10 0
+a 11 0
+e 12 13 14 15 16 17 18 19 20 0
+-1 -2 -12 0
+-1 2 12 0
+1 -2 12 0
+1 2 -12 0
+-12 -3 -13 0
+-12 3 13 0
+12 -3 13 0
+12 3 -13 0
+-13 -4 -14 0
+-13 4 14 0
+13 -4 14 0
+13 4 -14 0
+-14 -5 -15 0
+-14 5 15 0
+14 -5 15 0
+14 5 -15 0
+-15 -6 -16 0
+-15 6 16 0
+15 -6 16 0
+15 6 -16 0
+-16 -7 -17 0
+-16 7 17 0
+16 -7 17 0
+16 7 -17 0
+-17 -8 -18 0
+-17 8 18 0
+17 -8 18 0
+17 8 -18 0
+-18 -9 -19 0
+-18 9 19 0
+18 -9 19 0
+18 9 -19 0
+-19 -10 -20 0
+-19 10 20 0
+19 -10 20 0
+19 10 -20 0
+11 20 0
+-11 -20 0)";
+
+struct TestStruct {
+  TestStruct(std::string_view s, int td, const char* name)
+    : s(s)
+    , td(td)
+    , name(name) {}
+  std::string_view s;
+  int td;
+  std::string name;
+};
+
+static std::vector<TestStruct> TestsVec = {
+  TestStruct{ test_qdimacs_str, 2, "Small Test, td2" },
+  TestStruct{ test_qdimacs_str_longer, 3, "Slightly Larger Test, td3" },
+  TestStruct{ test_qdimacs_str_qbffam_parity_10,
+              10,
+              "Parity 10 from qbffam, td10" },
+  TestStruct{ test_qdimacs_str_qbffam_parity_10,
+              11,
+              "Parity 10 from qbffam, td11" },
+  TestStruct{ test_qdimacs_str_qbffam_parity_10,
+              12,
+              "Parity 10 from qbffam, td12" }
+};
+
+TEST_CASE("QBF formulas that go EXISTS 1 FORALL 2",
           "[integration][communicator][broker][solver][runner][solver_qbf]") {
-  ParacWorkerCountSetter workerCount(1);
+  ParacWorkerCountSetter workerCount(GENERATE(1, 2));
 
-  setenv("PARAC_MODULEPATH_SOLVER", "./modules/solver_qbf/libparac_solver_qbf.so", 1);
-  ParacoobaMock master(1, test_qdimacs_str);
+  setenv("PARAC_MODULEPATH_SOLVER",
+         "./modules/solver_qbf/libparac_solver_qbf.so",
+         1);
+  auto t = GENERATE(from_range(TestsVec));
+  CAPTURE(t.name);
+  setenv("PARAC_TREE_DEPTH", std::to_string(t.td).c_str(), 1);
+  ParacoobaMock master(1, t.s.data());
   unsetenv("PARAC_MODULEPATH_SOLVER");
-
-  // Do NOT load the SAT solver module!
-  std::string solverName = master.modules[PARAC_MOD_SOLVER]->name;
-  REQUIRE(solverName != "cpp_cubetree_splitter");
-
-  master.getThreadRegistry().wait();
-
-  REQUIRE(master.exit_status == PARAC_UNSAT);
-}
-
-TEST_CASE("Multi threaded QBF formula that goes EXISTS 1 FORALL 2",
-          "[integration][communicator][broker][solver][runner][solver_qbf]") {
-  ParacWorkerCountSetter workerCount(2);
-
-  setenv("PARAC_MODULEPATH_SOLVER", "./modules/solver_qbf/libparac_solver_qbf.so", 1);
-  ParacoobaMock master(1, test_qdimacs_str);
-  unsetenv("PARAC_MODULEPATH_SOLVER");
+  unsetenv("PARAC_TREE_DEPTH");
 
   // Do NOT load the SAT solver module!
   std::string solverName = master.modules[PARAC_MOD_SOLVER]->name;
