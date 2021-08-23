a6b0c34de4244dca89e1b21c66f83d5c7fe81d3f
Max Heisinger <mail@maximaximal.com>
2021-08-23 12:01:42 +0000

    Improve usage of simplified config setter structs in tests

--- test/test_connect_daemon_and_client.cpp
+++ test/test_connect_daemon_and_client.cpp
@@ -85,16 +85,13 @@ a -1 0
 
 TEST_CASE("Connect multiple daemon and one master without workers.",
           "[integration,communicator,broker,solver,runner]") {
-  setenv("PARAC_WORKER_COUNT", "0", 1);
+  ParacWorkerCountSetter workerCount0(0);
   ParacoobaMock master(1, test_dimacs_str);
-  setenv("PARAC_WORKER_COUNT", "1", 1);
+  ParacWorkerCountSetter workerCount1(1);
   ParacoobaMock daemon1(2, nullptr, &master);
   ParacoobaMock daemon2(3, nullptr, &master);
   ParacoobaMock daemon3(4, nullptr, &master);
   ParacoobaMock daemon4(5, nullptr, &master);
-  setenv("PARAC_WORKER_COUNT",
-         std::to_string(std::thread::hardware_concurrency()).c_str(),
-         1);
 
   master.getThreadRegistry().wait();
 
@@ -104,17 +101,14 @@ TEST_CASE("Connect multiple daemon and one master without workers.",
 TEST_CASE(
   "Connect multiple daemon and one master without workers using cadical cubes",
   "[integration,communicator,broker,solver,runner]") {
-  setenv("PARAC_CADICAL_CUBES", "1", 1);
-  setenv("PARAC_WORKER_COUNT", "0", 1);
+  ParacConfigSetter<size_t> cadicalCubes("PARAC_CADICAL_CUBES", 1);
+  ParacWorkerCountSetter workerCount0(0);
   ParacoobaMock master(1, test_dimacs_str);
-  setenv("PARAC_WORKER_COUNT", "1", 1);
+  ParacWorkerCountSetter workerCount1(1);
   ParacoobaMock daemon1(2, nullptr, &master);
   ParacoobaMock daemon2(3, nullptr, &master);
   ParacoobaMock daemon3(4, nullptr, &master);
   ParacoobaMock daemon4(5, nullptr, &master);
-  setenv("PARAC_WORKER_COUNT",
-         std::to_string(std::thread::hardware_concurrency()).c_str(),
-         1);
 
   master.getThreadRegistry().wait();
 
